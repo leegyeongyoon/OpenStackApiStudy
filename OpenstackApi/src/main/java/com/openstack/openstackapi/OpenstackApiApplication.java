3595c0bddba3b866f00c0732750afaac4b201945
igyeong-yun <gy.lee@okestro.com>
2022-09-20 18:17:15 +0900

    [feat] first commit

---
+++ OpenstackApi/src/main/java/com/openstack/openstackapi/OpenstackApiApplication.java
@@ -0,0 +1,13 @@
+package com.openstack.openstackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenstackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenstackApiApplication.class, args);
+    }
+
+}
---
+++ OpenstackApi/src/test/java/com/openstack/openstackapi/OpenstackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openstack.openstackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenstackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
