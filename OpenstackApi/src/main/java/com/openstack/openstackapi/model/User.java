67c07b9ec02c1b1049b9d9729ffa7dd178c6f34c
igyeong-yun <gy.lee@okestro.com>
2022-09-21 18:10:30 +0900

    [fix] jpa 자동 생성 시 테이블이 없어서 에러 난 부분 properties와 entity 추가

--- OpenstackApi/src/main/java/com/openstack/openstackapi/model/User.java
+++ OpenstackApi/src/main/java/com/openstack/openstackapi/model/User.java
@@ -7,12 +7,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Collection;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "TB_USER")
+@Entity
 public class User implements UserDetails {
 
     @Id
